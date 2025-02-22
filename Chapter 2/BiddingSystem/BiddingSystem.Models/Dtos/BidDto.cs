30662013b2f5a1d9b7497c7899874c4026cd1921
Reza <r.ghochkhani@gmail.com>
2025-02-22 11:43:31 -0500

    Add models to monolith project

---
+++ Chapter 2/BiddingSystem/BiddingSystem.Models/Dtos/BidDto.cs
@@ -0,0 +1,37 @@
+﻿using BiddingSystem.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiddingSystem.Models.Dtos
+{
+	public class BidDto
+	{
+		[Required]
+		public int BidId { get; set; }
+
+		[Required]
+		public int AuctionId { get; set; }
+
+		[Required]
+		public int UserId { get; set; }
+
+		[DataType(DataType.Currency)]
+		public decimal Amount { get; set; }
+
+		public DateTime BidTime { get; set; }
+
+		public Bid ToModel() {
+			return new Bid {
+				BidId = BidId,
+				AuctionId = AuctionId,
+				UserId = UserId,
+				Amount = Amount,
+				BidTime = BidTime
+			};
+		}
+	}
+}
---
+++ Chapter 2/BiddingSystem/BiddingSystem.Models/Entities/Bid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiddingSystem.Models.Entities
+{
+	public class Bid
+	{
+		public int BidId { get; set; }
+		public int AuctionId { get; set; }
+		public int UserId { get; set; }
+		public decimal Amount { get; set; }
+		public DateTime BidTime { get; set; }
+	}
+}
